4cf8d9b73bf44b157a991be1abc7cba4ee8c7bf3
Zac Smith <zrs3141592@gmail.com>
2016-07-19 09:21:05 -0600

    Added enemy sprite

--- space_blaster.rb
+++ space_blaster.rb
@@ -1,20 +1,26 @@
 require 'gosu'
 require_relative 'player'
+require_relative 'enemy'
 
 class SpaceBlaster < Gosu::Window
+  WIDTH = 800
+  HEIGHT = 600
   def initialize
-    super(800, 600)
+    super(WIDTH, HEIGHT)
     self.caption = 'Space Blaster'
     @player = Player.new(self)
+    @enemy = Enemy.new(self)
   end
   def update
     @player.turn_left if button_down?(Gosu::KbLeft)
     @player.turn_right if button_down?(Gosu::KbRight)
     @player.accelerate if button_down?(Gosu::KbUp)
     @player.move
+    @enemy.move
   end
   def draw
     @player.draw
+    @enemy.draw
   end
 
 
